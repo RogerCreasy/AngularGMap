d3782ee4467f1777218d08bd8d26b5d4a976b8f0
Roger Creasy <roger.creasy@gmail.com>
2015-04-20 11:40:08 -0400

    finalize revert corrections to working

--- scripts/golfApp.js
+++ scripts/golfApp.js
@@ -3,5 +3,21 @@
  * roger.creasy@gmail.com
  */
 
-var golfApp = angular.module('golfApp', ['golfControllers']);
+var golfApp = angular.module('golfApp', ['golfControllers', 'ngRoute']);
 
+golfApp.config(function($routeProvider) {
+    'use strict';
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'partials/home.html'
+        })
+        .when('/map', {
+            templateUrl: 'partials/map.html'
+        })
+        .when('/', {
+            templateUrl: 'partials/map.html'
+        })
+        .otherwise({
+            redirectTo: '/map'
+        });
+});
