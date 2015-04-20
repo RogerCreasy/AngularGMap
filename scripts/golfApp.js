647b0148063581124750a313696b3aa5f3b63d0c
Roger Creasy <roger.creasy@gmail.com>
2015-04-20 10:18:01 -0400

    get revert working, reverted index and golfApp

--- scripts/golfApp.js
+++ scripts/golfApp.js
@@ -1,39 +1,86 @@
 /**
- * Created by Roger Creasy on 4/17/2015.
+ * Created by roger creasy on 4/14/2015.
  * roger.creasy@gmail.com
  */
-var golfApp = angular.module('golfApp', [
-    'ngRoute',
-    'golfControllers'
-]);
 
-golfApp.config(function($routeProvider) {
-        'use strict';
-        $routeProvider
-            .when('/home', {
-                templateUrl: 'partials/home.html'
-            })
-            .when('/map', {
-                templateUrl: 'partials/map.html'
-            })
-            .when('/', {
-                templateUrl: 'partials/map.html'
-            })
-            .otherwise({
-                redirectTo: '/map'
-            });
-    });
+//Angular App Module and Controller
+angular.module('golfApp', [])
+    .controller('MapCtrl', function ($scope, $http) {
 
-golfApp.directive('mapCanvas', function() {
-        return {
-            link: function(scope, element) {
-                var mapOptions = {
-                    zoom: 11,
-                    center: new google.maps.LatLng(36.095580028219, -79.444059371948242),
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                };
-                map = new google.maps.Map(document.getElementById('map-canvas'),
-                    mapOptions);
+        $http.get('data/places.json').success(function(data) {
+            $scope.places = data;
+
+        var mapOptions = {
+            zoom: 11,
+            center: new google.maps.LatLng(36.095580028219, -79.444059371948242),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        $scope.markers = [];
+
+        var infoWindow = new google.maps.InfoWindow();
+
+        var createMarker = function (info){
+
+            if (info.locationType == 'course'){
+                var img = 'img/pin.png';
             }
-        };
+            if (info.locationType == 'biz'){
+                var img = 'img/storefront.png';
+            }
+            if (info.locationType == 'nichols'){
+                var img = 'img/logoNichols-sm.png';
+            }
+            //TODO add other location types, perhaps a switch statement
+
+            var marker = new google.maps.Marker({
+                map: $scope.map,
+                position: new google.maps.LatLng(info.lat, info.long),
+                title: info.place,
+                icon: img
+            });
+            marker.content = '<div>' +
+                    info.desc +
+                    '<br />' +
+                    '<a href="tel:+1' + info.phone + '">' + info.phone +
+            '</div>';
+
+            google.maps.event.addListener(marker, 'click', function(){
+                infoWindow.setContent('<h4>' + marker.title + '</h4>' + marker.content);
+                infoWindow.open($scope.map, marker);
+            });
+
+            $scope.markers.push(marker);
+
+        }
+
+        for (i = 0; i < $scope.places.length; i++){
+            createMarker($scope.places[i]);
+        }
+
+        google.maps.Map.prototype.clearMarkers = function() {
+            if (infoWindow) {
+                infoWindow.close();
+            }
+        }
+
+        $scope.openInfoWindow = function(e, selectedMarker){
+            e.preventDefault();
+            google.maps.event.trigger(selectedMarker, 'click');
+            $scope.map.setCenter(selectedMarker.internalPosition);
+            $scope.map.setZoom(16);
+
+        }
+
+        $scope.restartMap = function(e){
+            e.preventDefault();
+            $scope.map.clearMarkers();
+            $scope.map.setCenter({lat:36.095580028219, lng:-79.444059371948242});
+            $scope.map.setZoom(11);
+        }
+
+
+    });
     });
